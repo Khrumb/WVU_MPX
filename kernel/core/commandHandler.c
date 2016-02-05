daad44ff1856f3244ab872500ad5b5351a697e0c
ksprendki <ksprendki@localhost>
2016-02-05 01:38:50 -0500

    Finished date commands

--- kernel/core/commandHandler.c
+++ kernel/core/commandHandler.c
@@ -1,5 +1,3 @@
-
-/* the command handler */
 #include <stdint.h>
 #include <string.h>
 #include <system.h>
@@ -10,22 +8,11 @@
 
 int shutdown = 0;
 int a =0;
-int hour;
-int min;
-int sec;
-
 
 void version(){
   serial_println("Version: MODULE_R1");
 }
 
-/*!
-  * @class
-  * function name: turnOff
-  *  Description: shuts down mpx machine
-  * Parameters: takes in a char of y or n
-  * Returns: shutdown command
-*/
 void turnOff(){
   serial_print("Are you sure you want to shutdown (y/n):");
   char c[2];
@@ -41,15 +28,8 @@ void turnOff(){
      break;
    }
   }
-
 }
 
-/*!
-  function name: help
-  Description: shows the user what commands they have access to
-  Parameters: takes in a string
-  Returns: list of commands for the user
-*/
 
 void help(){
   serial_println("Command List:");
@@ -62,74 +42,265 @@ void help(){
   serial_println(" help - displays a list of commands and their uses.");
 }
 
-/*!
-  function name: strlen
-  Description: finds the length of a string
-  Parameters: takes in a char
-  Returns: string length
-*/
-
-int BCDtoD(int bcd){
-  int sum = 0;
-  int factor = 1;
-  do{
-    sum += (bcd%10)*factor;
-    factor = factor *2;
-    bcd = bcd/10;
-  }while(bcd > 0);
-  return sum;
+char* itoa(int val, int base){
+  static char buf[32] = {0};
+	int i = 30;
+	for(; val && i ; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+	return &buf[i+1];
 }
 
-void itos(int num, char *number){
-
-  int i = 1;
-  //+48
-  do{
-    number[i] = (num%10)+48;
-    i--;
-    num = num/10;
-  }while(num > 0);
-  number[2] = '\0';
+char BCDtoDec(char bcd){
+  return (((bcd & 0xF0) >> 4) * 10) + (bcd & 0x0F);
 }
 
-/*!
-  function name: getTime
-  Description: prints the time out for the user
-  Parameters: void
-  Returns: the time
-*/
+char DectoBCD(int dec){
+  return ((dec / 10) << 4) + (dec % 10);
+}
+
+char *formatNum(char *num){
+  char* num1 = strtok(num, " ");
+  if(!strcmp(num, "\0")){
+    return "00";
+  } else if(!strcmp(num1, "1\0")){
+    return "01";
+  } else if(!strcmp(num1, "2\0")){
+    return "02";
+  } else if(!strcmp(num1, "3\0")){
+    return "03";
+  } else if(!strcmp(num1, "4\0")){
+    return "04";
+  } else if(!strcmp(num, "5\0")){
+    return "05";
+  } else if(!strcmp(num1, "6\0")){
+    return "06";
+  } else if(!strcmp(num1, "7\0")){
+    return "07";
+  } else if(!strcmp(num1, "8\0")){
+    return "08";
+  } else if(!strcmp(num1, "9\0")){
+    return "09";
+  } else
+    return num;
+}
 
 void getTime(){
   outb(0x70, 0x04);
-  hour = inb(0x71);
+  char hour = inb(0x71);
   outb(0x70, 0x02);
-  min = inb(0x71);
+  char min = inb(0x71);
   outb(0x70, 0x00);
-  sec = inb(0x71);
-  hour = BCDtoD(hour);
-  min = BCDtoD(min);
-  sec = BCDtoD(sec);
-  char number[3];
-  itos(hour, number);
-  serial_println(number);
+  char sec = inb(0x71);
+  hour = BCDtoDec(hour);
+  min = BCDtoDec(min);  
+  sec = BCDtoDec(sec);
+  char *h, *numHours;
+  h = itoa(hour, 10);
+  numHours = formatNum(h);
+  serial_print(numHours);
+  serial_print(":");
+  char *m, *numMins;
+  m = itoa(min, 10);
+  numMins = formatNum(m);
+  serial_print(numMins);
+  serial_print(":");
+  char *s, *numSecs;
+  s = itoa(sec, 10);
+  numSecs = formatNum(s);
+  serial_print(numSecs);
+  serial_println("");
+}
+
+int asciiToDec(char num){
+  switch(num){
+    case 48:
+	return 0;
+	break;
+    case 49:
+	return 1;
+	break;
+    case 50:
+	return 2;
+	break;
+    case 51:
+	return 3;
+	break;
+    case 52:
+	return 4;
+	break;
+    case 53:
+	return 5;
+	break;
+    case 54:
+	return 6;
+	break;
+    case 55:
+	return 7;
+	break;
+    case 56:
+	return 8;
+	break;
+    case 57:
+	return 9;
+	break;
+  }
+  return -1; 
 }
 
 
-
-void test(){
+int parseTime(char buffer[]){
+  int tens = asciiToDec(buffer[0]);
+  int ones = asciiToDec(buffer[1]);
+  int h = tens * 10 + ones;
+  if(h<0 || h>12)
+    return 1;
+  char hour = DectoBCD(h);
+  tens = asciiToDec(buffer[3]);
+  ones = asciiToDec(buffer[4]);
+  int m = tens * 10 + ones;
+  if(m<0 || m>60)
+    return 1;
+  char min = DectoBCD(m);
+  tens = asciiToDec(buffer[6]);
+  ones = asciiToDec(buffer[7]);
+  int s = tens * 10 + ones;
+  if(s<0 || s>60)
+    return 1;
+  char sec = DectoBCD(s);
+  cli();
   outb(0x70, 0x04);
-  a = inb(0x71);
-  //char number[3];
-  //itos(temp, number);
-  //serial_println(number);
+  outb(0x71, hour);
+  outb(0x70, 0x02);
+  outb(0x71, min);
+  outb(0x70, 0x00);
+  outb(0x71, sec);
+  sti();
+  return 0;
 }
 
-/*!
-  function name: parseCommand
-  Description: parses the users input to be sent to the command handler
-  Parameters: a string (command from the user)
-  Returns: the correct command
-*/
+void setTime(){
+  serial_println("Enter the time in the format 00:00:00");
+  char buffer[400];
+  char c[2];
+  c[0] = 0;
+  c[1] = '\0';
+  int index = 0;
+  int input = 1;
+  serial_print("> ");
+  while(input==1){
+   if (inb(COM1+5)&1){
+     c[0] = inb(COM1);
+     if(c[0]==13){
+         serial_println("");
+	 if(parseTime(buffer)==0){
+	   input=0;
+	 }
+	 else{
+	   serial_println("Error invalid time. Please enter a valid time in the format 00:00:00");
+	   serial_print("> ");
+	 }
+         do{
+           buffer[index] = '\0';
+         }while(index-- > 0);
+         index = 0;
+     }
+     else{
+	buffer[index++] = c[0];
+        serial_print(c);
+     }
+   }
+  }
+}
+
+void getDate(){
+  outb(0x70, 0x07);
+  char day = inb(0x71);
+  outb(0x70, 0x08);
+  char month = inb(0x71);
+  outb(0x70, 0x09);
+  char year = inb(0x71);
+  day = BCDtoDec(day);
+  month = BCDtoDec(month);  
+  year = BCDtoDec(year);
+  char *m, *numMonths;
+  m = itoa(month, 10);
+  numMonths = formatNum(m);
+  serial_print(numMonths);
+  serial_print("/");
+  char *d, *numDays;
+  d = itoa(day, 10);
+  numDays = formatNum(d);
+  serial_print(numDays);
+  serial_print("/");
+  char *y, *numYears;
+  y = itoa(year, 10);
+  numYears = formatNum(y);
+  serial_print(numYears);
+  serial_println("");
+}
+
+int parseDate(char buffer[]){
+  int tens = asciiToDec(buffer[0]);
+  int ones = asciiToDec(buffer[1]);
+  int m = tens * 10 + ones;
+  if(m<1 || m>12)
+    return 1;
+  char month = DectoBCD(m);
+  tens = asciiToDec(buffer[3]);
+  ones = asciiToDec(buffer[4]);
+  int d = tens * 10 + ones;
+  if(d<1 || d>31)
+    return 1;
+  char day = DectoBCD(d);
+  tens = asciiToDec(buffer[6]);
+  ones = asciiToDec(buffer[7]);
+  int y = tens * 10 + ones;
+  char year = DectoBCD(y);
+  cli();
+  outb(0x70, 0x07);
+  outb(0x71, day);
+  outb(0x70, 0x08);
+  outb(0x71, month);
+  outb(0x70, 0x09);
+  outb(0x71, year);
+  sti();
+  return 0;
+}
+
+
+void setDate(){
+  serial_println("Enter the date in the format 01/01/01");
+  char buffer[400];
+  char c[2];
+  c[0] = 0;
+  c[1] = '\0';
+  int index = 0;
+  int input = 1;
+  serial_print("> ");
+  while(input==1){
+   if (inb(COM1+5)&1){
+     c[0] = inb(COM1);
+     if(c[0]==13){
+         serial_println("");
+	 if(parseDate(buffer)==0){
+	   input=0;
+	 }
+	 else{
+	   serial_println("Error invalid date. Please enter a valid date in the format 00/00/00");
+	   serial_print("> ");
+	 }
+         do{
+           buffer[index] = '\0';
+         }while(index-- > 0);
+         index = 0;
+     }
+     else{
+	buffer[index++] = c[0];
+        serial_print(c);
+     }
+   }
+  }
+}
 
 void parseCommand(char* command){
   char* com1 = strtok(command, " ");
@@ -142,24 +313,15 @@ void parseCommand(char* command){
   } else if(!strcmp(com1, "gettime\0")){
     getTime();
   } else if(!strcmp(com1, "settime\0")){
-    help();
+    setTime();
   } else if(!strcmp(com1, "getdate\0")){
-    help();
+    getDate();
   } else if(!strcmp(com1, "setdate\0")){
-    help();
-  } else if(!strcmp(com1, "test\0")){
-    test();
+    setDate();
   } else {
-    serial_println("Invalid command. User 'help' to get a complete list." );
+    serial_println("Invalid command. Use 'help' to get a complete list." );
   }
 }
-
-/*!
-  function name: commandHandler
-  Description: executes the function selected by the user
-  Parameters: takes in a command
-  Returns: the correct result of the desired fucntion, i.e. the time if getTime is requested
-*/
 
 void commandHandler(){
   char buffer[400];
@@ -174,7 +336,6 @@ void commandHandler(){
      switch(c[0]){
        case 13:
          serial_println("");
-         //insert command handler here
          parseCommand(buffer);
          serial_print("> ");
          do{
