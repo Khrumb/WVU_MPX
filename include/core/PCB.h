93d262f3a1f42d16cd3d0b08275124e10aaa91ef
ksprendki <ksprendki@localhost>
2016-02-26 00:54:33 -0500

    Fixed bug with deleting first item in queue and added function headers

--- include/core/PCB.h
+++ include/core/PCB.h
@@ -41,18 +41,65 @@ typedef struct pcb{
   struct pcb *prev;
 } pcb;
 
+/**
+ * function name: init_queues
+ * Description: calls sys_alloc_mem() to allocate  memory for both queues
+ * Paramaters: None
+ * Valid return: ok code
+ * Invalid return: null (error)
+*/
 int init_queues();
 
+/**
+ * function name: AllocatePCB
+ * Description: calls sys_alloc_mem() to allocate  memory for process
+ * Paramaters: None
+ * Valid return: pcb pointer
+ * Invalid return: null (error)
+*/
 struct pcb* AllocatePCB();
 
+/**
+ * function name: FreePCB
+ * Description: calls sys_free_mem() to free all memory associated with a given PCB
+ * Parameters: pcb pointer
+ * Valid return: ok code
+ * Invalid return: error code
+*/
 int FreePCB(struct pcb* block);
 
+/**
+ * function name: SetupPCB
+ * Description: calls allocatePCB, initializes the a pcb, sets pcb to a ready state
+ * Parameters: process name, class, and priority
+ * Valid return: PCB pointer
+ * Invalid return: null for error or invalid input message
+*/
 struct pcb* SetupPCB(char* name, unsigned int class, unsigned int priority);
 
+/**
+ * function name: FindPCB
+ * Description: searches both queues for a process with the given name
+ * Parameters: process name
+ * Valid return: PCB pointer
+ * Invalid return: null if PCB does not exist
+*/
 struct pcb* FindPCB(char* name);
 
+/**
+ * function name: InsertPCB
+ * Description: puts PCB into correct queue
+ * Returns: none
+*/
 void InsertPCB(struct pcb* block);
 
+/**
+ * function name: RemovePCB
+ * Description: removes PCB from queue it is currently stored in
+ * Parameters: pointer to PCB
+ * Valid return: confirmation message
+ * Invalid return: null (PCB not found)
+*/
 int RemovePCB(struct pcb* block);
 
 #endif
