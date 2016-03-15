ff381304bcdd5d8158fb3d0a893f71b51891ff83
jrgregg <jrgregg@localhost>
2016-03-15 15:20:08 -0400

    expanding syscall

--- kernel/core/sys_call.c
+++ kernel/core/sys_call.c
@@ -2,11 +2,29 @@
 #include "modules/mpx_supt.h"
 
 #include <core/sys_call.h>
+#include <core/PCB.h>
 
-u32int ret;
+context* caller;
+
+pcb* cop;
 
 u32int* sys_call(context *registers)
 {
-ret = registers->eax;
-return &ret;
+	if(cop == NULL){
+		caller = registers;
+	} else {
+		if(params->op_code == IDLE){
+			//reassign stacktop of COP
+			caller = registers;
+
+		}
+		if(params->op_code == EXIT){
+			RemovePCB(cop);
+		}
+	}
+	if(ready->count != 0){
+		cop=ready->head;
+		return (u32int*)cop->stack_top;
+	}
+	return (u32int*)caller;
 }
