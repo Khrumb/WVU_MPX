907c3b25236774440e5f8b3f1eef37828ad682e7
jrgregg <jrgregg@localhost>
2016-03-15 14:51:03 -0400

    Added a few shindigs

--- include/core/sys_call.h
+++ include/core/sys_call.h
@@ -1,6 +1,12 @@
 #ifndef _SYS_CALL_H
 #define _SYS_CALL_H
 
-u32int* sys_call(context* registers);
+typedef struct  {
+u32int gs , fs , es , ds ;
+u32int edi , esi , ebp , esp , ebx , edx , ecx , eax ;
+u32int eip , cs , eflags ;
+} context;
+
+u32int* sys_call(context *registers);
 
 #endif
